214eee7550931f70f8466c9af288713ffc3d462c
muhindi guchu <votratravis@gmail.com>
2025-03-20 20:50:10 +0300

    Adding individual html pages for each product

---
+++ javascript/main.js
@@ -0,0 +1,24 @@
+document.getElementById("yeezyb").onclick = () => {
+    window.location.href = 'index2.html'; 
+}
+document.getElementById("pink").onclick = () => {
+    window.location.href = 'index3.html'; 
+}
+document.getElementById("vans").onclick = () => {
+    window.location.href = 'index4.html'; 
+}
+document.getElementById("samba").onclick = () => {
+    window.location.href = 'index5.html'; 
+}
+document.getElementById("puma").onclick = () => {
+    window.location.href = 'index6.html'; 
+}
+document.getElementById("brown").onclick = () => {
+    window.location.href = 'index7.html'; 
+}
+document.getElementById("yeezy").onclick = () => {
+    window.location.href = 'index8.html'; 
+}
+document.getElementById("dior").onclick = () => {
+    window.location.href = 'index9.html'; 
+}
