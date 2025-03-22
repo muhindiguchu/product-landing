31c7cc9b5fc2818adfb5339005e9e80015fbec3a
muhindi guchu <votratravis@gmail.com>
2025-03-22 13:44:29 +0300

    Adding the ability to Add to cart

---
+++ javascript/cart8.js
@@ -0,0 +1,33 @@
+window.onload = () => {
+    let product = "Air Jordan 1 High Dior";
+    let price = 2000;
+    document.getElementById("product").innerHTML = "product :"+ " " + product  ;
+    document.getElementById("price").innerHTML = "price :"+ " " + price  ;
+    document.getElementById("quantity").innerHTML = "quantity :"+ " WIP"   ;
+    document.getElementById("size").innerHTML = "Size :"+ " WIP"   ;
+    document.getElementById("Total").innerHTML = "Total :"+ " WIP"  ;
+
+}
+function checkLocation(){
+    let location = document.querySelector("select").value;
+    if(location == "CBD"){
+        let shipping = 0;
+        document.getElementById("shipping").innerHTML = "Shipping :"+ " " + shipping  ;
+}else if(location == "Nairobi"){
+    let shipping = 100;
+    document.getElementById("shipping").innerHTML = "Shipping :"+ " " + shipping  ;
+
+}else if(location == "Ruiru"){
+    let shipping = 150;
+    document.getElementById("shipping").innerHTML = "Shipping :"+ " " + shipping  ;
+
+}else if(location == "Kisumu"){
+    let shipping = 500;
+    document.getElementById("shipping").innerHTML = "Shipping :"+ " " + shipping  ;
+
+}else if(location == "Mombasa"){
+    let shipping = 500;
+    document.getElementById("shipping").innerHTML = "Shipping :"+ " " + shipping  ;
+
+}
+}
